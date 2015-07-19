9c74b8af033d673ce2c469467181aae665755413
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2015-07-19 12:21:09 +0200

    added source maps and most recent version in package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,25 @@
 var path = require('path');
+var webpack = require('webpack');
+
 module.exports = {
     //entry: './es6/module/mainImportAs.js',
-    entry: './es6/module/main.js',
+    //entry: './es6/module/main.js',
+    entry: './es6/arrow-function/arrow.js',
     output: {
         path: __dirname,
         filename: 'bundle.js'
     },
     module: {
         loaders: [
-            { test: path.join(__dirname, 'es6'), loader: 'babel-loader' }
+            {test: path.join(__dirname, 'es6'), loader: 'babel-loader'}
         ]
-    }
+    },
+    plugins: [
+        new webpack.NoErrorsPlugin()
+    ],
+    stats: {
+        colors: true
+    },
+    devtool: 'source-map'
+
 };
